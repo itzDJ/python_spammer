3b48e14df6ba851c1cadb8383bce6967648bc49a
itzDJ <74993463+itzDJ@users.noreply.github.com>
2021-01-11 15:23:08 -0500

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -1,16 +1,9 @@
 import pyautogui
 import time
 
-y = 0
-time.sleep(1)
-order = input("The time has come...\n")
-if order == "Execute order 66...":
+run == True
+if run:
     time.sleep(2)
-    while y != -1:
-        y += 1
         pyautogui.typewrite("legit")
         pyautogui.press("enter")
-        print(y)
         time.sleep(0.5)
-else:
-    print("Wrong order...")
